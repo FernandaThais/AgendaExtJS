5bc33ad99ddd9d1040a0b7c709c96f9e00c90b20
Fernanda Thais Fogaca Monteiro <fernanda.tfmonteiro@gmail.com>
2017-05-12 18:08:53 -0300

    Simplificado o app inicial.

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -9,10 +9,6 @@ Ext.define('AgendaExtJS.view.main.MainController', {
 
     alias: 'controller.main',
 
-    onItemSelected: function (sender, record) {
-        Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
-    },
-
     onConfirm: function (choice) {
         if (choice === 'yes') {
             //
